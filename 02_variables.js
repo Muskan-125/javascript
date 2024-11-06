d4bdb86478b8a4418afb7e85f7006726114d167b
Muskan-125 <muskansharmax124@gmail.com>
2024-11-06 13:39:02 +0530

    edited readme

---
+++ 02_variables.js
@@ -0,0 +1,18 @@
+const accountId = 15678
+let accountEmail = "muskan@gmail.com"
+var accountPassword = "123456"
+accountCity = "ghaziabad"
+let accountState;
+
+// accountId = 2 // not allowed as constant variable
+
+accountEmail = "muskan123@gmail.com"
+accountPassword = "15678"
+accountCity = "Delhi"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
